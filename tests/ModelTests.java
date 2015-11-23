5ac60937855564082fceaa057037e1282401b0e6
Jona Stubbe <dev@jona-stubbe.de>
2015-11-23 12:56:16 +0100

    ModelTests

--- tests/ModelTests.java
+++ tests/ModelTests.java
@@ -1,2 +1,11 @@
+import model.LagerHalle;
+import org.junit.Test;
+
 public class ModelTests {
+	@Test
+	public void lager() {
+		LagerHalle halle = new LagerHalle(10);
+		assert halle.getKapazität() == 10;
+		assert halle.getBestand() == 0;
+	}
 }
