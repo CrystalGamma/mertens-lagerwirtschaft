9e61a34faced52a7315f60545f17ea79fa91daec
Jona Stubbe <dev@jona-stubbe.de>
2015-11-24 12:41:21 +0100

    LagerView, transaktions-test

--- tests/ModelTests.java
+++ tests/ModelTests.java
@@ -1,7 +1,11 @@
 import model.LagerHalle;
 import model.LagerNichtVollGenug;
 import model.LagerÜbervoll;
+import model.Model;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ModelTests {
 	@Test
@@ -46,5 +50,14 @@ public class ModelTests {
 		if (noexcept)
 			assert false;
 		assert halle.getBestand() == 5;
+	}
+	@Test
+	public void transaktion() {
+		Model model = new Model();
+		Model.LagerView halle1 = model.getLager()[0].getUnterLager()[0].getUnterLager()[0];
+		Map<Model.LagerView, Integer> buchungen = new HashMap<>();
+		buchungen.put(halle1, 10);
+		model.übernehmeLieferung(buchungen, "2015-11-24");
+		assert halle1.getBestand() == 10;
 	}
 }
