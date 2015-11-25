134bf1763860a568b8e49206f5cc8409cd55da04
Jona Stubbe <dev@jona-stubbe.de>
2015-11-25 14:17:43 +0100

    Teste ungültige Transaktion; Platzsparen

--- tests/ModelTests.java
+++ tests/ModelTests.java
@@ -22,16 +22,14 @@ public class ModelTests {
 		} catch (LagerNichtVollGenug e) {
 			noexcept = false;
 		}
-		if (noexcept)
-			assert false;
+		assert !noexcept;
 		noexcept = true;
 		try {
 			halle.buchen_(-10);
 		} catch (LagerNichtVollGenug e) {
 			noexcept = false;
 		}
-		if (noexcept)
-			assert false;
+		assert !noexcept;
 		assert halle.getBestand() == 0;
 		halle.dryRunBuchung(5);
 		halle.buchen_(5);
@@ -42,25 +40,35 @@ public class ModelTests {
 		} catch (LagerÜbervoll e) {
 			noexcept = false;
 		}
-		if (noexcept)
-			assert false;
+		assert !noexcept;
 		noexcept = true;
 		try {
 			halle.buchen_(10);
 		} catch (LagerÜbervoll e) {
 			noexcept = false;
 		}
-		if (noexcept)
-			assert false;
+		assert !noexcept;
 		assert halle.getBestand() == 5;
 	}
 	@Test
 	public void transaktion() {
 		Model model = new Model();
 		Model.LagerHalle halle1 = (Model.LagerHalle)model.getLager()[0].getUnterLager()[0].getUnterLager()[0];
+		assert halle1.getBestand() == 0;
 		Map<Model.LagerHalle, Integer> buchungen = new HashMap<>();
 		buchungen.put(halle1, 10);
 		model.übernehmeLieferung(buchungen, "2015-11-24");
 		assert halle1.getBestand() == 10;
-	}
+		assert model.getBuchungenFürHalle(halle1).get("2015-11-24").get(halle1) == 10;
+		Map<Model.LagerHalle, Integer> ungültig = new HashMap<>();
+		ungültig.put(halle1, -15);
+		boolean noexcept = true;
+		try {
+			model.übernehmeLieferung(ungültig, "2015-11-25");
+		} catch (LagerNichtVollGenug e) {
+			noexcept = false;
+		}
+		assert !noexcept;
+		assert halle1.getBestand() == 10;
+ 	}
 }
