b9b05abc22b55936afbd126441ae5d7a150fc796
Florian Bussmann <hallo@florian-bussmann.de>
2015-12-01 12:09:30 +0100

    Demodaten

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -8,6 +8,10 @@ import ui.LagerAnsicht;
 import ui.StartAnsicht;
 import ui.Zulieferung;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+
 public class Controller {
     Model model;
 
@@ -17,7 +21,53 @@ public class Controller {
 
     public Controller() {
         this.model = new Model();
+        this.generateDummyData();
         new StartAnsicht(this.model, this);
+    }
+
+
+    public void generateDummyData() {
+        Map<LagerHalle, Integer> buchung;
+        Vector<LagerHalle> lagerHallen = new Vector<>();
+
+        int day = 1;
+        int amount = 15;
+        for (Model.Lager tier1 : model.getLager()) {
+            if (tier1 instanceof LagerHalle) {
+                lagerHallen.add((LagerHalle) tier1);
+                continue;
+            }
+
+            for (Model.Lager tier2 : tier1.getUnterLager()) {
+                if (tier2 instanceof LagerHalle) {
+                    lagerHallen.add((LagerHalle) tier2);
+                    continue;
+                }
+
+                for (Model.Lager tier3 : tier2.getUnterLager())
+                    if (tier3 instanceof LagerHalle)
+                        lagerHallen.add((LagerHalle) tier3);
+            }
+        }
+
+
+        for (LagerHalle lagerHalle : lagerHallen) {
+            buchung = new HashMap<>();
+            buchung.put(lagerHalle, +amount);
+            model.übernehmeLieferung(buchung, "2015-10-" + (day < 10 ? "0" + day : day));
+            day++;
+            amount++;
+        }
+
+        amount = -5;
+        day = 1;
+        for (LagerHalle lagerHalle : lagerHallen) {
+            buchung = new HashMap<>();
+            buchung.put(lagerHalle, amount);
+            model.übernehmeLieferung(buchung, "2015-11-" + (day < 10 ? "0" + day : day));
+            day++;
+            amount--;
+        }
     }
 
     public Model getModel() {
