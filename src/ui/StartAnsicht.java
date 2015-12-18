9fbe80f02c8c4622683a5236b24af9a9953be2b7
hwgfp <Westhof.dev@gmail.com>
2015-12-18 11:02:13 +0100

    buttons gone

--- src/ui/StartAnsicht.java
+++ src/ui/StartAnsicht.java
@@ -74,10 +74,6 @@ public class StartAnsicht extends JFrame implements Observer {
 		StartansichtTableModel defaultModel= new StartansichtTableModel(tableData, columnNames); 
 		table= new JTable(defaultModel);
 		table.getTableHeader().setReorderingAllowed(false);
-		//Button einfügen
-		table.getColumn("").setCellRenderer(new ButtonRenderer());
-		ButtonEditor buttonEditor=new ButtonEditor(new JCheckBox());
-		table.getColumn("").setCellEditor(buttonEditor);
 		//Hinzufügen der Tabelle und Header an das Tabellenpanel
 		tablePanel.add(table.getTableHeader());
 		tablePanel.add(table);
@@ -139,7 +135,6 @@ public class StartAnsicht extends JFrame implements Observer {
 						Model.Lager lager=LagerNameZuLager.get(vectorAusgewählteZeile.get(1).toString().trim());
 						if(lager instanceof Model.OberLager )
 						{
-							buttonEditor.fireEditingStopped();
 							LagerZuklappen.put(lager, !(LagerZuklappen.get(lager)));
 							update(null, null);
 						}
